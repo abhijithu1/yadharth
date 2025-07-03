ce3ad3387a3aececc088667b0c3f898123383dff
vagabond-0 <220229@tkmce.ac.in>
2025-07-04 01:54:58 +0530

    make uniform supabase

--- src/app/api/Event/route.ts
+++ src/app/api/Event/route.ts
@@ -1,11 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createClient } from '@supabase/supabase-js';
-
-// Initialize Supabase client
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
+import { supabase } from "@/utils/supabaseClient";
 export async function POST(req: NextRequest) {
   try {
     // Parse the request body
